a0800f790b80f2e8a84c27be3303cdf3b9584f60
Sergio Sanchez <sergiosan2802@gmail.com>
2022-03-04 01:34:22 -0500

    feat: update style button component

--- src/components/button/Style.ts
+++ src/components/button/Style.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { IButtonStyle } from "../../interfaces/buttonStyle";
-import { colorPrimary, colorQuartiary, colorTertiary } from "../../styles/variables";
+import { borderRadiusSM, colorPrimary, colorQuartiary, colorTertiary, paddingSM } from "../../styles/variables";
 
 
 export const ButtonStyleComponent = styled.button<IButtonStyle> `
   background-color: ${ props => props.disabled ? colorQuartiary : colorPrimary};
   border: none;
-  border-radius: 0.5rem;
+  border-radius: ${borderRadiusSM};
   color: ${colorTertiary};
   cursor: pointer;
-  padding: 0.5rem;
+  padding: ${paddingSM};
 `;
