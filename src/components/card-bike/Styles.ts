db2ffb334276e2ece9440ba149f64f78556123b0
Sergio Sanchez <sergiosan2802@gmail.com>
2022-03-03 22:04:33 -0500

    feat: added new components styles card bike & add method bike service

--- src/components/card-bike/Styles.ts
+++ src/components/card-bike/Styles.ts
@@ -1,8 +1,9 @@
-import styled from "styled-components";
-import { colorPrimary, colorTertiary } from "../../styles/style.global";
+import styled, { keyframes } from "styled-components";
+import { colorDanger, colorPrimary, colorSecondary, colorTertiary } from "../../styles/style.global";
 
 export const BikeCard = styled.div `
   background-color: ${colorTertiary};
+  border: solid thin #e9e9e9;
   border-radius: 1rem;
   display: flex;
   gap: 1rem;
@@ -10,16 +11,47 @@ export const BikeCard = styled.div `
 `;
 
 export const BikeImgCard = styled.img `
-  width: 100px;
-  height: 100px;
+  object-fit: cover;
   border-radius: 1rem;
+  height: 100px;
+  width: 100px;
 `;
 
 export const BikeContentCard = styled.div `
-  display: grid;
-  gap: 0.5rem;
+  /* display: grid;
+  gap: 0.5rem; */
 `;
 
 export const BikeTitleCard = styled.h4 `
   color: ${colorPrimary};
+`;
+
+
+const SpinnerAnimation = keyframes`
+ 0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const Spinner = styled.div `
+  animation: ${SpinnerAnimation} 1s linear infinite;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #09f;
+  border-radius: 50%;
+  height: 56px;
+  width: 56px;
+`;
+
+export const ListEmpty = styled.p `
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const TextError = styled.p `
+  color: ${colorDanger};
+  font-size: 18px;
+  text-align: center;
 `;
--- src/services/bikeServices.tsx
+++ src/services/bikeServices.tsx
@@ -15,7 +15,7 @@ export class BikesServices {
     this.apiUrl = REACT_APP_URL_BIKES;
   }
 
-  async getBikesStolen({ page, perPage, distance, stolenness }: IGetBikeStolen): Promise<IGetBikeResponse | undefined> {
+  async getBikesStolen({ page, perPage, distance, stolenness }: IGetBikeStolen): Promise<IGetBikeResponse> {
     const request = {
       params: {
         page,
@@ -24,21 +24,13 @@ export class BikesServices {
         stolenness
       }
     }
-    try {
-      const { data } = await Axios.get(`${this.apiUrl}`, request)
-      return data;
-    } catch (error) {
-      console.log('error: ', error);
-    }
+    const { data } = await Axios.get(`${this.apiUrl}`, request)
+    return data;
   }
 
-  async getAllBikesCount(): Promise<IGetBikeCountResponse | undefined> {
-    try {
-      const { data } = await Axios.get(`${this.apiUrl}/count`)
-      return data;
-    } catch (error) {
-      console.log('error: ', error);
-    }
+  async getAllBikesCount(): Promise<IGetBikeCountResponse> {
+    const { data } = await Axios.get(`${this.apiUrl}/count`)
+    return data;
   }
 
 }
