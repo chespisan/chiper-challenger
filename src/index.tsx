b444d065a02b7b1f1571de70459aed5a44ab9de3
Sergio Sanchez <sergiosan2802@gmail.com>
2022-03-03 22:02:53 -0500

    feat: remove link style

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { AppRouter } from './routes/AppRouter';
 import { GlobalStyles } from './styles/style.global';
-import './styles/style.global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
