1b19b58f7d525047cbcf93335ff734e7263001a1
Sergio Sanchez <sergiosan2802@gmail.com>
2022-03-04 01:33:23 -0500

    feat: update variables css

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,11 +1,24 @@
 // COLORS //
 
-export const colorPrimary = '#3498db';
+export const colorPrimary   = '#3498db';
 export const colorSecondary = '#f2f2f2';
-export const colorTertiary = '#fff';
+export const colorTertiary  = '#fff';
 export const colorQuartiary = '#878787';
-export const colorWarning = '#efc13b';
-export const colorDanger = '#e74c3c';
+export const colorQuintiary = '#dbdbdb'
+export const colorWarning   = '#efc13b';
+export const colorDanger    = '#e74c3c';
 
 
 // STRUCTURE //
+export const paddingSM = '0.5rem';
+export const paddingMD = '1rem';
+
+export const marginSM = '0.5rem';
+export const marginMD = '1rem';
+export const marginLG = '2rem';
+
+export const borderRadiusSM = '0.5rem';
+export const borderRadiusMD = '1rem';
+
+export const fontSizeSM = '12px';
+export const fontSizeMD = '18px';
